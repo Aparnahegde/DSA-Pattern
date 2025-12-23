83ff8e7648b0c974baf7425cec4f8422b30569a2
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2025-12-23 11:57:08 -0500

    Add solution for 'Container With Most Water' problem
    Implement the two-pointer approach to find the maximum area of water that can be contained.

---
+++ Two-Pointers/container_with water.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/container-with-most-water/description/
+class Solution {
+    public int maxArea(int[] height) {
+   int left=0,right=height.length-1;
+        int maxArea=0;
+        while(left<right){
+            int currentmax=(right-left)*Math.min(height[left],height[right]);
+            maxArea=Math.max(maxArea,currentmax);
+            if(height[left]<height[right])left++;
+            else right--;
+        }
+        return maxArea;
+    }
+}
