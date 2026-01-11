d162992ef1cc7f5b484042e828515fb8feb65443
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-11 02:18:28 -0500

    Add solution for Boats to Save People problem
    Implement solution for the 'Boats to Save People' problem by sorting the array and using a two-pointer technique to count the number of boats needed.

---
+++ Two-Pointers/BoatsToSavePeople.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/boats-to-save-people/description/
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int first=0;
+        int b=0;
+        int last=people.length-1;
+        while(first<=last){
+            if(people[first]+people[last]<=limit){
+                first++;  
+            }
+            last--;
+             b++;
+        }
+        return b;
+    }
+}
