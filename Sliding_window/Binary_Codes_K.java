3a0f41b82e0a944d4bd26f886ae8bfa07aa10f2c
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-16 12:45:36 -0500

    Add solution for binary codes check
    Implement a solution to check if a string contains all binary codes of size k.

---
+++ Sliding_window/Binary_Codes_K.java
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+
+        Set<String> seen = new HashSet<>();
+
+        int required = 1 << k;
+
+        for (int i = 0; i <= s.length() - k; i++) {
+
+            String sub = s.substring(i, i + k);
+
+            seen.add(sub);
+
+            if (seen.size() == required) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
