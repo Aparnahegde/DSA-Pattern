6986a558e5136ae21d7e1329bc7b6a7dafa65305
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-18 01:31:56 -0500

    Create Max_Conc_Ones.java

---
+++ Sliding_window/Max_Conc_Ones.java
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/max-consecutive-ones-iii/
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int z=0;
+        int s=0;
+        int o=0;
+        for(int e=0;e<nums.length;e++){
+            if(nums[e]==0){
+                z++;
+            }
+            while(z>k){
+                if(nums[s]==0)
+                    z--;
+                    s++;
+            }
+            o=Math.max(o,e-s+1);
+        }
+        return o;
+    }
+}
