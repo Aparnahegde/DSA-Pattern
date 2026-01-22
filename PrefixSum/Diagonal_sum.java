1618a0cf02e4e825d6f4e77b740fd6405c7d4759
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-22 12:12:20 -0500

    Add diagonal sum method to Solution class
    Implement diagonal sum calculation for a matrix.

---
+++ PrefixSum/Diagonal_sum.java
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/matrix-diagonal-sum/description/
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            // add primary diagonal
+            sum = sum + mat[i][i];
+
+            // add secondary diagonal
+            if (i != n - 1 - i) {   
+                sum = sum + mat[i][n - 1 - i];
+            }
+        }
+
+        return sum;
+    }
+}
