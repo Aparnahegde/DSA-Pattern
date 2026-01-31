c6b70cab38e0c869f7ebf7ac84572296060aa72c
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-31 00:27:20 -0500

    Update reverseVowels.java

--- StringPattern/reverseVowels.java
+++ StringPattern/reverseVowels.java
@@ -26,3 +26,48 @@ class Solution {
         return new String(arr);
     }
 }
+
+
+
+
+//Optimised approach
+
+class Solution {
+    public String reverseVowels(String s) {
+
+        // Store vowels in a Set for fast lookup
+        Set<Character> vowels = new HashSet<>();
+        for (char c : "AEIOUaeiou".toCharArray()) {
+            vowels.add(c);
+        }
+
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            // Move left pointer until vowel found
+            while (left < right && !vowels.contains(arr[left])) {
+                left++;
+            }
+
+            // Move right pointer until vowel found
+            while (left < right && !vowels.contains(arr[right])) {
+                right--;
+            }
+
+            // Swap vowels
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
+
+    
