b1b930348b5b52b20edbfbcaff74a90b83af2395
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-02-02 12:13:37 -0500

    Implement equalPairs method to count matching pairs

---
+++ HashMapAndHashSet/EqualRowAndColumn.java
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/equal-row-and-column-pairs/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        Map<List<Integer>, Integer> map = new HashMap<>();
+
+        // Store rows
+        for (int i = 0; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(grid[i][j]);
+            }
+            map.put(row, map.getOrDefault(row, 0) + 1);
+        }
+
+        int count = 0;
+
+    
+        for (int i = 0; i < n; i++) {
+            List<Integer> col = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                col.add(grid[j][i]);
+            }
+            count += map.getOrDefault(col, 0);
+        }
+
+        return count;
+    }
+}
